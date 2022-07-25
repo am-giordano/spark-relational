e2603137dc50bad6de1d71fef5b3ae56dfef003e
am-giordano <am.giordano.carmena@gmail.com>
2022-07-25 21:41:41 +0200

    refactoring

--- src/main/scala/com/amgiordano/spark/relational/NameComposer.scala
+++ src/main/scala/com/amgiordano/spark/relational/NameComposer.scala
@@ -1,12 +1,6 @@
 package com.amgiordano.spark.relational
 
 object NameComposer {
-
-  def compose(args: String*): String = {
-    args.mkString("!!")
-  }
-
-  def indexName(entityName: String): String = {
-    compose(entityName, "__id__")
-  }
+  def compose(args: String*): String = args.mkString("!!")
+  def indexName(entityName: String): String = compose(entityName, "__id__")
 }
