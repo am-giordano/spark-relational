00ab4f4d91bd10350b715755be613aa1259caae8
am-giordano <am.giordano.carmena@gmail.com>
2022-08-04 19:25:39 +0200

    more functional-style Tabulator

--- src/main/scala/com/amgiordano/spark/relational/Tabulator.scala
+++ src/main/scala/com/amgiordano/spark/relational/Tabulator.scala
@@ -4,50 +4,53 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{col, explode}
 import org.apache.spark.sql.types.{ArrayType, StructType}
 
-private class Tabulator(entityName: String, var df: DataFrame, foreignKeys: Array[String]) {
-
-  type TripletArray = Array[(String, DataFrame, Array[String])]
-
-  val primaryKey: String = NameComposer.indexName(entityName)
-  val allKeys: Array[String] = Array(primaryKey) ++ foreignKeys
-
-  def tabulateGo(): (DataFrame, TripletArray) = {
-    if (checkNoObjects) {
-      val fromArrayTriplets = arrayColumns.map(colName => (colName, extractArrayColumn(colName), allKeys))
-      (df, fromArrayTriplets)
-    } else {
-      flattenOneLevel()
-      tabulateGo()
-    }
-  }
-
-  def checkNoObjects: Boolean = df.schema.count(_.dataType.isInstanceOf[StructType]) == 0
-
-  def flattenOneLevel(): Unit = {
-    for (struct <- df.schema.filter(_.dataType.isInstanceOf[StructType])) {
-      val colName = struct.name
-      for (fieldName <- struct.dataType.asInstanceOf[StructType].fields.map(_.name)) {
-        df = df.withColumn(NameComposer.compose(colName, fieldName), col(s"$colName.$fieldName"))
-      }
-      df = df.drop(colName)
-    }
-  }
-
-  def arrayColumns: Array[String] = df.schema.filter(_.dataType.isInstanceOf[ArrayType]).map(_.name).toArray
-
-  def extractArrayColumn(colName: String): DataFrame = {
-    val dfNew = df.select(allKeys.map(col) ++ Array(explode(col(colName)).as(colName)): _*)
-    df = df.drop(colName)
-    dfNew
-  }
-}
+import scala.annotation.tailrec
 
 object Tabulator {
 
-  type TripletArray = Array[(String, DataFrame, Array[String])]
+  type EntityTriplet = (String, DataFrame, Array[String])
 
-  def tabulate(entityName: String, df: DataFrame, foreignKeys: Array[String]): (DataFrame, TripletArray) = {
-    val tab = new Tabulator(entityName, df, foreignKeys)
-    tab.tabulateGo()
+  def tabulate(entityName: String, df: DataFrame, foreignKeys: Array[String]): (DataFrame, Array[EntityTriplet]) = {
+
+    val primaryKey = NameComposer.indexName(entityName)
+    val allKeys = Array(primaryKey) ++ foreignKeys
+
+    @tailrec
+    def go(df: DataFrame): (DataFrame, Array[EntityTriplet]) = {
+      if (checkNoObjects(df)) {
+        val arrCols = arrayColumns(df)
+        (df.drop(arrCols: _*), arrCols.map(colName => tripletFromArrayColumn(df, colName, allKeys)))
+      } else {
+        go(flattenOneLevel(df))
+      }
+    }
+
+    go(df)
+  }
+
+  def checkNoObjects(df: DataFrame): Boolean = df.schema.count(_.dataType.isInstanceOf[StructType]) == 0
+
+  def flattenOneLevel(df: DataFrame): DataFrame = {
+    var dfFlat = df
+    for (struct <- dfFlat.schema.filter(_.dataType.isInstanceOf[StructType])) {
+      val colName = struct.name
+      for (fieldName <- struct.dataType.asInstanceOf[StructType].fields.map(_.name)) {
+        dfFlat = dfFlat.withColumn(NameComposer.compose(colName, fieldName), col(s"$colName.$fieldName"))
+      }
+      dfFlat = dfFlat.drop(colName)
+    }
+    dfFlat
+  }
+
+  def arrayColumns(df: DataFrame): Array[String] = {
+    df.schema.filter(_.dataType.isInstanceOf[ArrayType]).map(_.name).toArray
+  }
+
+  def tripletFromArrayColumn(df: DataFrame, colName: String, allKeys: Array[String]): EntityTriplet = {
+    (colName, explodeArrayColumn(df, colName, allKeys), allKeys)
+  }
+
+  def explodeArrayColumn(df: DataFrame, colName: String, allKeys: Array[String]): DataFrame = {
+    df.select(allKeys.map(col) ++ Array(explode(col(colName)).as(colName)): _*)
   }
 }
